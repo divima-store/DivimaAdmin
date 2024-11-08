4c62ff9d6c4c227de377d23909e8e7a10e94c47d
Abdolah3320 <130370108+Abdolah3320@users.noreply.github.com>
2024-11-08 22:36:14 +0200

    customer search by id and email implementted

--- app/customers/CustomerCard .js
+++ app/customers/CustomerCard .js
@@ -5,7 +5,7 @@ export default function CustomerCard({ customer }) {
     <div className="bg-white text-gray-950 shadow-md rounded-lg overflow-hidden w-full max-w-sm">
       <div className="p-4">
         <h3 className="text-lg font-semibold mb-2">{customer.name}</h3>
-        <h3 className="text-lg font-semibold mb-2">customerid : {customer.id}</h3>
+        <h3 className="text-lg font-semibold mb-2">customer id : {customer.id}</h3>
         <p className="text-sm text-gray-500 mb-4">{customer.email}</p>
         <div className="bg-gray-100 p-3 rounded-md">
           <h4 className="font-medium mb-2">Address:</h4>
